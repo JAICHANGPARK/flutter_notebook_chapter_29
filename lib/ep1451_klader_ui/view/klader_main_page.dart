80d988eb3de6bb7554da8d0bdaba13fb14f9820a
JAICHANGPARK <aristojeff@gmail.com>
2023-03-10 17:09:35 +0900

    Create klader_main_page.dart

---
+++ lib/ep1451_klader_ui/view/klader_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class KladerMainPage extends StatefulWidget {
+  const KladerMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<KladerMainPage> createState() => _KladerMainPageState();
+}
+
+class _KladerMainPageState extends State<KladerMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
