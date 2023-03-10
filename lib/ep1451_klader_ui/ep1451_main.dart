483a3412498f873afc03b4e3c95bfb321a40cf9e
JAICHANGPARK <aristojeff@gmail.com>
2023-03-10 17:07:18 +0900

    Create ep1451_main.dart

---
+++ lib/ep1451_klader_ui/ep1451_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(KladerUI());
+}
+
+class KladerUI extends StatelessWidget {
+  const KladerUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
