01845eb700094d1a7049f935eaf0f25fec37da00
JAICHANGPARK <aristojeff@gmail.com>
2023-03-10 17:14:11 +0900

    Update klader_main_page.dart

--- lib/ep1451_klader_ui/view/klader_main_page.dart
+++ lib/ep1451_klader_ui/view/klader_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class KladerMainPage extends StatefulWidget {
   const KladerMainPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,39 @@ class KladerMainPage extends StatefulWidget {
 class _KladerMainPageState extends State<KladerMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Klader"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_bag_outlined),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [Container()],
+          ),
+          Positioned(
+              bottom: 24,
+              left: 16,
+              right: 16,
+              child: Container(
+                height: 80,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: StadiumBorder(),
+                ),
+                child: Row(
+                  children: [],
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
