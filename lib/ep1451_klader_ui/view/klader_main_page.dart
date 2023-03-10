aba7257191a259d2f6bb0a7c1a8596734f11a80f
JAICHANGPARK <aristojeff@gmail.com>
2023-03-10 17:14:33 +0900

    Update klader_main_page.dart

--- lib/ep1451_klader_ui/view/klader_main_page.dart
+++ lib/ep1451_klader_ui/view/klader_main_page.dart
@@ -15,6 +15,7 @@ class _KladerMainPageState extends State<KladerMainPage> {
         title: Text("Klader"),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
+        elevation: 0,
         actions: [
           IconButton(
             onPressed: () {},
@@ -22,6 +23,7 @@ class _KladerMainPageState extends State<KladerMainPage> {
           ),
         ],
       ),
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           Column(
