b7c32fe8602210725c84084deb149bd47c16f360
JAICHANGPARK <aristojeff@gmail.com>
2023-03-12 10:55:26 +0900

    Update learning_app_main_page.dart

---
+++ lib/ep1453_learning_app/view/learning_app_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LearningAppMainPage extends StatefulWidget {
+  const LearningAppMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<LearningAppMainPage> createState() => _LearningAppMainPageState();
+}
+
+class _LearningAppMainPageState extends State<LearningAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
