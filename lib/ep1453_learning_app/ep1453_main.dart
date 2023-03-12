d0ffcdef79730356342b64fb7da501fa3b4de80d
JAICHANGPARK <aristojeff@gmail.com>
2023-03-12 10:53:32 +0900

    Update ep1453_main.dart

---
+++ lib/ep1453_learning_app/ep1453_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LearningAppDesign extends StatelessWidget {
+  const LearningAppDesign({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
