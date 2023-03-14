7c4e2e80ab55797a70fe9f4a7ee17bac96f7fa2b
JAICHANGPARK <aristojeff@gmail.com>
2023-03-14 20:25:54 +0900

    Create learning_app_detail_page.dart

---
+++ lib/ep1453_learning_app/view/learning_app_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LearningAppDetailPage extends StatefulWidget {
+  const LearningAppDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<LearningAppDetailPage> createState() => _LearningAppDetailPageState();
+}
+
+class _LearningAppDetailPageState extends State<LearningAppDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
