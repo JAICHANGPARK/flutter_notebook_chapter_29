43b4e9eede63480d4f40a46bcf00f4efd976bbd4
JAICHANGPARK <aristojeff@gmail.com>
2023-03-15 23:09:40 +0900

    Update audio_podcasts_main_page.dart

--- lib/ep1456_audio_podcasts_app/view/audio_podcasts_main_page.dart
+++ lib/ep1456_audio_podcasts_app/view/audio_podcasts_main_page.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class AudioPodcastsMainPage extends StatefulWidget {
+  const AudioPodcastsMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<AudioPodcastsMainPage> createState() => _AudioPodcastsMainPageState();
+}
+
+class _AudioPodcastsMainPageState extends State<AudioPodcastsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.apps),
+        ),
+        actions: [
+          CircleAvatar(),
+        ],
+      ),
+    );
+  }
+}
