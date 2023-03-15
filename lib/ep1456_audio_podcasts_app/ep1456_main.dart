3e4c0de1acc45b1b69c77312e17eb9d5e462e94a
JAICHANGPARK <aristojeff@gmail.com>
2023-03-15 23:11:25 +0900

    Update ep1456_main.dart

--- lib/ep1456_audio_podcasts_app/ep1456_main.dart
+++ lib/ep1456_audio_podcasts_app/ep1456_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_29/ep1456_audio_podcasts_app/view/audio_podcasts_main_page.dart';
 
 void main() {
-  runApp(AudioPodcastsApp());
+  runApp(const AudioPodcastsApp());
 }
 
 class AudioPodcastsApp extends StatelessWidget {
@@ -12,7 +12,7 @@ class AudioPodcastsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/": (context) => AudioPodcastsMainPage(),
+        "/": (context) => const AudioPodcastsMainPage(),
       },
     );
   }
