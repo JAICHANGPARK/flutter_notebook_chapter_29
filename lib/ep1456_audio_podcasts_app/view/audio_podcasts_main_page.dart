f86af32fd4a3d51cb2c4b285fb748c62d0a84871
JAICHANGPARK <aristojeff@gmail.com>
2023-03-15 23:13:17 +0900

    Update audio_podcasts_main_page.dart

--- lib/ep1456_audio_podcasts_app/view/audio_podcasts_main_page.dart
+++ lib/ep1456_audio_podcasts_app/view/audio_podcasts_main_page.dart
@@ -12,12 +12,21 @@ class _AudioPodcastsMainPageState extends State<AudioPodcastsMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         leading: IconButton(
           onPressed: () {},
           icon: Icon(Icons.apps),
         ),
         actions: [
           CircleAvatar(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.mic_none_outlined), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.explore_outlined), label: "Explore"),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmarks_rounded), label: "Saved"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "Profile"),
         ],
       ),
     );
