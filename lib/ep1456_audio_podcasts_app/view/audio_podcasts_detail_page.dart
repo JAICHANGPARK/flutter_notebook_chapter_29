87767f4d38b6f9e8d2a67ddef798d0ca64aca3eb
JAICHANGPARK <aristojeff@gmail.com>
2023-03-17 16:57:15 +0900

    Update audio_podcasts_detail_page.dart

---
+++ lib/ep1456_audio_podcasts_app/view/audio_podcasts_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AudioPodcastsDetailPage extends StatefulWidget {
+  const AudioPodcastsDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<AudioPodcastsDetailPage> createState() => _AudioPodcastsDetailPageState();
+}
+
+class _AudioPodcastsDetailPageState extends State<AudioPodcastsDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
