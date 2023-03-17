f1ba9d67e748198107fc14a3aa25db29e7a35e5e
JAICHANGPARK <aristojeff@gmail.com>
2023-03-17 16:59:51 +0900

    Update audio_podcasts_detail_page.dart

--- lib/ep1456_audio_podcasts_app/view/audio_podcasts_detail_page.dart
+++ lib/ep1456_audio_podcasts_app/view/audio_podcasts_detail_page.dart
@@ -11,14 +11,19 @@ class _AudioPodcastsDetailPageState extends State<AudioPodcastsDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        elevation: 0,
         title: Text("Details"),
         actions: [
           IconButton(
             onPressed: () {},
             icon: Icon(Icons.download),
-          )
+          ),
         ],
+        centerTitle: true,
       ),
       body: Column(
         children: [],
