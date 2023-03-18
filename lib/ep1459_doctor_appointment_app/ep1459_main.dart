ea84b1451038e36baa19c5985dd9e6217ce3dae1
JAICHANGPARK <aristojeff@gmail.com>
2023-03-18 10:39:56 +0900

    Update ep1459_main.dart

--- lib/ep1459_doctor_appointment_app/ep1459_main.dart
+++ lib/ep1459_doctor_appointment_app/ep1459_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_29/ep1459_doctor_appointment_app/view/doctor_appointment_home_page.dart';
 
 void main() {
-  runApp(DoctorAppointmentApp());
+  runApp(const DoctorAppointmentApp());
 }
 
 class DoctorAppointmentApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class DoctorAppointmentApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: DoctorAppointmentHomePage(),
     );
   }
