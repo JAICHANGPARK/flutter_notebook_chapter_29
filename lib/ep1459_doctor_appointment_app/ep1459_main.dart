590b218715fe5bc6181378010f581e8962bcc5ed
JAICHANGPARK <aristojeff@gmail.com>
2023-03-18 10:38:12 +0900

    Create ep1459_main.dart

---
+++ lib/ep1459_doctor_appointment_app/ep1459_main.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class DoctorAppointmentApp extends StatelessWidget {
+  const DoctorAppointmentApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
