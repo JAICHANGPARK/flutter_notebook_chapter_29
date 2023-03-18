0a289429f3c48bd24f8ae081ddf10c45960b7ec7
JAICHANGPARK <aristojeff@gmail.com>
2023-03-18 10:39:21 +0900

    Update doctor_appointment_home_page.dart

--- lib/ep1459_doctor_appointment_app/view/doctor_appointment_home_page.dart
+++ lib/ep1459_doctor_appointment_app/view/doctor_appointment_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class DoctorAppointmentHomePage extends StatefulWidget {
   const DoctorAppointmentHomePage({Key? key}) : super(key: key);
 
@@ -11,6 +10,6 @@ class DoctorAppointmentHomePage extends StatefulWidget {
 class _DoctorAppointmentHomePageState extends State<DoctorAppointmentHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
