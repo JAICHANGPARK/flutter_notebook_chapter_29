4233a8fb92e6ec52c7c3855273c6ae72c22d02f1
JAICHANGPARK <aristojeff@gmail.com>
2023-03-20 16:47:53 +0900

    Create doctor_list_page.dart

---
+++ lib/ep1459_doctor_appointment_app/view/doctor_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DoctorListPage extends StatefulWidget {
+  const DoctorListPage({Key? key}) : super(key: key);
+
+  @override
+  State<DoctorListPage> createState() => _DoctorListPageState();
+}
+
+class _DoctorListPageState extends State<DoctorListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
