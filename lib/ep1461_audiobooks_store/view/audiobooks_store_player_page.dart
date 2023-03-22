a4b7fb76b0d2e8be88e339cabfdb2b2848b2ea77
JAICHANGPARK <aristojeff@gmail.com>
2023-03-22 21:09:17 +0900

    Create audiobooks_store_player_page.dart

---
+++ lib/ep1461_audiobooks_store/view/audiobooks_store_player_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AudiobooksStorePlayerPage extends StatefulWidget {
+  const AudiobooksStorePlayerPage({Key? key}) : super(key: key);
+
+  @override
+  State<AudiobooksStorePlayerPage> createState() => _AudiobooksStorePlayerPageState();
+}
+
+class _AudiobooksStorePlayerPageState extends State<AudiobooksStorePlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
