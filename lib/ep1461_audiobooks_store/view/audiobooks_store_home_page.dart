ed473673ddcc93de2c67cf25363a44410a5b79c7
JAICHANGPARK <aristojeff@gmail.com>
2023-03-22 21:09:20 +0900

    Create audiobooks_store_home_page.dart

---
+++ lib/ep1461_audiobooks_store/view/audiobooks_store_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AudioBooksStoreHomePage extends StatefulWidget {
+  const AudioBooksStoreHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<AudioBooksStoreHomePage> createState() => _AudioBooksStoreHomePageState();
+}
+
+class _AudioBooksStoreHomePageState extends State<AudioBooksStoreHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
