86537a914f178cbc7a7a373b844a46d388bf6ed0
JAICHANGPARK <aristojeff@gmail.com>
2023-03-24 16:52:00 +0900

    Update flight_booking_home_page.dart

--- lib/ep1465_flight_booking_app/view/flight_booking_home_page.dart
+++ lib/ep1465_flight_booking_app/view/flight_booking_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_29/ep1465_flight_booking_app/view/flight_booking_select_page.dart';
 
 class FlightBookingHomePage extends StatefulWidget {
   const FlightBookingHomePage({Key? key}) : super(key: key);
@@ -13,7 +14,13 @@ class _FlightBookingHomePageState extends State<FlightBookingHomePage> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {  },
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => FlightBookingSelectPage(),
+              ),
+            );
+          },
           child: Text("Search"),
         ),
       ),
