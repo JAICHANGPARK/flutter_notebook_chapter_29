1930a9faf4c8168fa7c7194514a7964c3fb34a3a
JAICHANGPARK <aristojeff@gmail.com>
2023-03-24 16:48:14 +0900

    Update ep1465_main.dart

--- lib/ep1465_flight_booking_app/ep1465_main.dart
+++ lib/ep1465_flight_booking_app/ep1465_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_29/ep1465_flight_booking_app/view/flight_booking_home_page.dart';
 
 void main() {
   runApp(FlightBookingApp());
@@ -9,6 +10,8 @@ class FlightBookingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FlightBookingHomePage(),
+    );
   }
 }
