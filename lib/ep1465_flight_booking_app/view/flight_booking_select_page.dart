5e966cc0a8eb6bed181d998e3f80c31bbfdf01fd
JAICHANGPARK <aristojeff@gmail.com>
2023-03-24 16:46:50 +0900

    Update flight_booking_select_page.dart

---
+++ lib/ep1465_flight_booking_app/view/flight_booking_select_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FlightBookingSelectPage extends StatefulWidget {
+  const FlightBookingSelectPage({Key? key}) : super(key: key);
+
+  @override
+  State<FlightBookingSelectPage> createState() => _FlightBookingSelectPageState();
+}
+
+class _FlightBookingSelectPageState extends State<FlightBookingSelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
