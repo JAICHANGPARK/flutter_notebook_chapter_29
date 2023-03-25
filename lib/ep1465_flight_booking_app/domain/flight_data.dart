33237e39ca274a4f73da1752b9ad4d37aeda4f2d
JAICHANGPARK <aristojeff@gmail.com>
2023-03-25 10:22:32 +0900

    Update flight_data.dart

--- lib/ep1465_flight_booking_app/domain/flight_data.dart
+++ lib/ep1465_flight_booking_app/domain/flight_data.dart
@@ -2,5 +2,15 @@ enum SeatType {
   available,
   booked,
   selected,
-  
+  aisle,
+}
+
+class FlightSeat {
+  String seatName;
+  SeatType seatType;
+
+  FlightSeat({
+    required this.seatName,
+    required this.seatType,
+  });
 }
