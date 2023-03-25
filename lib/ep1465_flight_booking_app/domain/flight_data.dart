225cfd6d59b88a69cbd76370ebb69d577d04a627
JAICHANGPARK <aristojeff@gmail.com>
2023-03-25 10:34:04 +0900

    Update flight_data.dart

--- lib/ep1465_flight_booking_app/domain/flight_data.dart
+++ lib/ep1465_flight_booking_app/domain/flight_data.dart
@@ -23,4 +23,20 @@ List<FlightSeat> flightSeatItems = [
   FlightSeat(seatName: "A4", seatType: SeatType.available),
   FlightSeat(seatName: "A5", seatType: SeatType.available),
   FlightSeat(seatName: "A6", seatType: SeatType.available),
+
+  FlightSeat(seatName: "B1", seatType: SeatType.available),
+  FlightSeat(seatName: "B2", seatType: SeatType.available),
+  FlightSeat(seatName: "B3", seatType: SeatType.available),
+  FlightSeat(seatName: "", seatType: SeatType.aisle),
+  FlightSeat(seatName: "B4", seatType: SeatType.available),
+  FlightSeat(seatName: "B5", seatType: SeatType.available),
+  FlightSeat(seatName: "B6", seatType: SeatType.available),
+
+  FlightSeat(seatName: "C1", seatType: SeatType.available),
+  FlightSeat(seatName: "C2", seatType: SeatType.available),
+  FlightSeat(seatName: "C3", seatType: SeatType.available),
+  FlightSeat(seatName: "", seatType: SeatType.aisle),
+  FlightSeat(seatName: "C4", seatType: SeatType.available),
+  FlightSeat(seatName: "C5", seatType: SeatType.available),
+  FlightSeat(seatName: "C6", seatType: SeatType.available),
 ];
