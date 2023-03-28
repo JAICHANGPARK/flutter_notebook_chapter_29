a8930e360bd2fc9f362c1f517ac6c7f4b1c6d4b9
JAICHANGPARK <aristojeff@gmail.com>
2023-03-28 18:33:17 +0900

    Update ep1469_main.dart

---
+++ lib/ep1469_news_app/ep1469_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
