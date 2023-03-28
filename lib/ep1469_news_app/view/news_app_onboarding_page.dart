8875c4acc5d7a300f510db6e3b7d74cf0801ec2e
JAICHANGPARK <aristojeff@gmail.com>
2023-03-28 18:35:43 +0900

    Create news_app_onboarding_page.dart

---
+++ lib/ep1469_news_app/view/news_app_onboarding_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewsAppOnboardingPage extends StatefulWidget {
+  const NewsAppOnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsAppOnboardingPage> createState() => _NewsAppOnboardingPageState();
+}
+
+class _NewsAppOnboardingPageState extends State<NewsAppOnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
