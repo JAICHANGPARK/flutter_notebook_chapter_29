e3bc024221cb6d49894c5ba46d28026bd6245a54
JAICHANGPARK <aristojeff@gmail.com>
2023-03-28 18:38:40 +0900

    Update news_app_onboarding_page.dart

--- lib/ep1469_news_app/view/news_app_onboarding_page.dart
+++ lib/ep1469_news_app/view/news_app_onboarding_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NewsAppOnboardingPage extends StatefulWidget {
   const NewsAppOnboardingPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,36 @@ class NewsAppOnboardingPage extends StatefulWidget {
 class _NewsAppOnboardingPageState extends State<NewsAppOnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/08/03/40/police-1807409_1280.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
