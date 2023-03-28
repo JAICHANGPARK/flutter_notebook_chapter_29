798de4021122a48eee9b9ba17fb169120aec8655
JAICHANGPARK <aristojeff@gmail.com>
2023-03-28 18:35:45 +0900

    Update ep1469_main.dart

--- lib/ep1469_news_app/ep1469_main.dart
+++ lib/ep1469_news_app/ep1469_main.dart
@@ -9,6 +9,8 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
