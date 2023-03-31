ee5afbdb0a2302e787ba042fdd4d0f6ab521ca70
JAICHANGPARK <aristojeff@gmail.com>
2023-04-01 07:37:00 +0900

    Create music_player_home_page.dart

---
+++ lib/ep1473_music_player_ui/view/music_player_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerHomePage extends StatefulWidget {
+  const MusicPlayerHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayerHomePage> createState() => _MusicPlayerHomePageState();
+}
+
+class _MusicPlayerHomePageState extends State<MusicPlayerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
