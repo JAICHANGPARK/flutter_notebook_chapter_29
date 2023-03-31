a664872273d0d247e56ead0bf269611abe9bd51a
JAICHANGPARK <aristojeff@gmail.com>
2023-03-31 20:18:54 +0900

    Update news_app_detail_page.dart

--- lib/ep1469_news_app/view/news_app_detail_page.dart
+++ lib/ep1469_news_app/view/news_app_detail_page.dart
@@ -12,16 +12,92 @@ class _NewsAppDetailPageState extends State<NewsAppDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
               Row(
                 children: [
                   CircleAvatar(
                     radius: 24,
-                  )
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                      ),
+                    ),
+                    backgroundColor: Colors.grey[200],
+                    foregroundColor: Colors.black,
+                  ),
+                  Spacer(),
+                  CircleAvatar(
+                    radius: 24,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(
+                        Icons.bookmark_border,
+                      ),
+                    ),
+                    backgroundColor: Colors.grey[200],
+                    foregroundColor: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  CircleAvatar(
+                    radius: 24,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(
+                        Icons.share,
+                      ),
+                    ),
+                    backgroundColor: Colors.grey[200],
+                    foregroundColor: Colors.black,
+                  ),
                 ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 12,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text("Dream Walker Mar 31, 2023 Sports")
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            "Lorem ipsum is placeholder text commonly used in the graphic",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               )
-          ],
+            ],
+          ),
         ),
       ),
     );
