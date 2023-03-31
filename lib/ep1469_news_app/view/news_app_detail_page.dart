18c35fbcb763a533e68b31113386231e26d6c39c
JAICHANGPARK <aristojeff@gmail.com>
2023-03-31 20:10:27 +0900

    Create news_app_detail_page.dart

---
+++ lib/ep1469_news_app/view/news_app_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewsAppDetailPage extends StatefulWidget {
+  const NewsAppDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsAppDetailPage> createState() => _NewsAppDetailPageState();
+}
+
+class _NewsAppDetailPageState extends State<NewsAppDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
