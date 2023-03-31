e79baaf6a1afb2d1e1e9f7f0d51d12d42e5a495c
JAICHANGPARK <aristojeff@gmail.com>
2023-04-01 07:35:02 +0900

    Update ep1473_main.dart

--- lib/ep1473_music_player_ui/ep1473_main.dart
+++ lib/ep1473_music_player_ui/ep1473_main.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+void main() {}
+
+class MusicPlayerUI extends StatelessWidget {
+  const MusicPlayerUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
