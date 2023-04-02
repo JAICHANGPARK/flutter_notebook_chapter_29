4062a9ee1df9735d062608a278c043b1f2009c3c
JAICHANGPARK <aristojeff@gmail.com>
2023-04-02 10:17:34 +0900

    Update ep1473_main.dart

--- lib/ep1473_music_player_ui/ep1473_main.dart
+++ lib/ep1473_music_player_ui/ep1473_main.dart
@@ -11,7 +11,7 @@ class MusicPlayerUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MusicPlayerHomePage(),
     );
   }
