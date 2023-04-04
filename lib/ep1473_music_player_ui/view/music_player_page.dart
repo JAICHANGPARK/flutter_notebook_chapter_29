bb84f993cbdb6caa05505427d5f48587e7b18d3e
JAICHANGPARK <aristojeff@gmail.com>
2023-04-04 18:33:41 +0900

    Update music_player_page.dart

--- lib/ep1473_music_player_ui/view/music_player_page.dart
+++ lib/ep1473_music_player_ui/view/music_player_page.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 
 
+class MusicPlayerPage extends StatefulWidget {
+  const MusicPlayerPage({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayerPage> createState() => _MusicPlayerPageState();
+}
+
+class _MusicPlayerPageState extends State<MusicPlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
