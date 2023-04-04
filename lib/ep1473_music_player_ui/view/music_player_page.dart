8bbdf15af38c8a85a64c6ed5f25e2baa01f73c00
JAICHANGPARK <aristojeff@gmail.com>
2023-04-04 18:36:42 +0900

    Update music_player_page.dart

--- lib/ep1473_music_player_ui/view/music_player_page.dart
+++ lib/ep1473_music_player_ui/view/music_player_page.dart
@@ -11,22 +11,34 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16)
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
-                )
-              ],
-            )
-          ],
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
