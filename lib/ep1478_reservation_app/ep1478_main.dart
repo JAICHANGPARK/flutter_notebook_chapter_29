f94d76058b4ad1560ee246f5406ffe410631aca0
JAICHANGPARK <aristojeff@gmail.com>
2023-04-06 19:23:17 +0900

    Create ep1478_main.dart

---
+++ lib/ep1478_reservation_app/ep1478_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ReservationApp extends StatelessWidget {
+  const ReservationApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
