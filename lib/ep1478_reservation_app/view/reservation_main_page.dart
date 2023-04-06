499c78109599ec31a7b9339b75406a4e77b87ec9
JAICHANGPARK <aristojeff@gmail.com>
2023-04-06 19:31:43 +0900

    Update reservation_main_page.dart

--- lib/ep1478_reservation_app/view/reservation_main_page.dart
+++ lib/ep1478_reservation_app/view/reservation_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ReservationMainPage extends StatefulWidget {
   const ReservationMainPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,52 @@ class ReservationMainPage extends StatefulWidget {
 class _ReservationMainPageState extends State<ReservationMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 58,
+                  width: 58,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Text(
+                  "Hello, ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Dream 👋",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  height: 58,
+                  width: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
