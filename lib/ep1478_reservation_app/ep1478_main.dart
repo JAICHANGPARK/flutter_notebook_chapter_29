65b42378bfee0f6b71c06bf90c52fb2a8f29fcf8
JAICHANGPARK <aristojeff@gmail.com>
2023-04-06 19:25:44 +0900

    Update ep1478_main.dart

--- lib/ep1478_reservation_app/ep1478_main.dart
+++ lib/ep1478_reservation_app/ep1478_main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class ReservationApp extends StatelessWidget {
-  const ReservationApp({Key? key}) : super(key: key);
+  ReservationApp({Key? key}) : super(key: key);
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        routes: [ShellRoute(routes: [
+
+        ])],
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
