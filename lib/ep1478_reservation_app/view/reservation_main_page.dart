c8ad6d982ac2c681474577e0de35f158f240888e
JAICHANGPARK <aristojeff@gmail.com>
2023-04-06 19:27:40 +0900

    Create reservation_main_page.dart

---
+++ lib/ep1478_reservation_app/view/reservation_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ReservationMainPage extends StatefulWidget {
+  const ReservationMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<ReservationMainPage> createState() => _ReservationMainPageState();
+}
+
+class _ReservationMainPageState extends State<ReservationMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
