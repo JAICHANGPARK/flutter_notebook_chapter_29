fc6e5c118d73458568a3fdab37b9df4b0712edd8
JAICHANGPARK <aristojeff@gmail.com>
2023-04-09 10:55:54 +0900

    Create task_management_main_page.dart

---
+++ lib/ep1481_task_management_app/view/task_management_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskManagementMainPage extends StatefulWidget {
+  const TaskManagementMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskManagementMainPage> createState() => _TaskManagementMainPageState();
+}
+
+class _TaskManagementMainPageState extends State<TaskManagementMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
