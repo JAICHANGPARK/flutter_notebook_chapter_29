ea61c1a50b46b1e13c89dadfdab5d1b21ca17c17
JAICHANGPARK <aristojeff@gmail.com>
2023-04-09 10:55:13 +0900

    Update ep1481_main.dart

---
+++ lib/ep1481_task_management_app/ep1481_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskManagementApp extends StatelessWidget {
+  const TaskManagementApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
