231c4f7e4a3044d3fa12be8d29b20c17883b9a77
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:39:14 +0900

    Update task_home_page.dart

--- lib/ep1484_task_app/view/task_home_page.dart
+++ lib/ep1484_task_app/view/task_home_page.dart
@@ -14,10 +14,16 @@ class _TaskAppHomePageState extends State<TaskAppHomePage> {
       backgroundColor: Color.fromRGBO(26, 26, 26, 1),
       body: Stack(
         children: [
-          SafeArea(
-              child: Column(
-            children: [],
-          )),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            top: 0,
+            child: SafeArea(
+                child: Column(
+              children: [],
+            )),
+          ),
           Positioned(
             bottom: 24,
             left: 0,
