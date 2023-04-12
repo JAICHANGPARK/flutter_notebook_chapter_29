a0bfa56423bd3518364d543a0573acb578761ced
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:22:12 +0900

    Update ep1484_task_app.dart

--- lib/ep1484_task_app/ep1484_task_app.dart
+++ lib/ep1484_task_app/ep1484_task_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Ep1484TaskApp());
+}
 
 class Ep1484TaskApp extends StatelessWidget {
   const Ep1484TaskApp({Key? key}) : super(key: key);
