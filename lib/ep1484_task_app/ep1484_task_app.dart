0dad7a8194dac62715a22ea43e1762dbd5209d8c
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:21:51 +0900

    Create ep1484_task_app.dart

---
+++ lib/ep1484_task_app/ep1484_task_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Ep1484TaskApp extends StatelessWidget {
+  const Ep1484TaskApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
