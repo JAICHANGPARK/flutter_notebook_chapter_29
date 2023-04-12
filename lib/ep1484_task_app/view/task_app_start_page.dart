80fa1226dfdf8e34d7725eeb0b1a082bfedd2303
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:36:56 +0900

    Update task_app_start_page.dart

--- lib/ep1484_task_app/view/task_app_start_page.dart
+++ lib/ep1484_task_app/view/task_app_start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_29/ep1484_task_app/view/task_home_page.dart';
 
 class TaskAppStartPage extends StatefulWidget {
   const TaskAppStartPage({Key? key}) : super(key: key);
@@ -63,32 +64,39 @@ class _TaskAppStartPageState extends State<TaskAppStartPage> {
                 ),
               ),
               const Spacer(),
-              Container(
-                decoration: BoxDecoration(
-                  color: const Color.fromRGBO(253, 244, 133, 1),
-                  borderRadius: BorderRadius.circular(48),
-                ),
-                padding: const EdgeInsets.only(
-                  left: 32,
-                  top: 4,
-                  bottom: 4,
-                  right: 4,
-                ),
-                margin: const EdgeInsets.only(bottom: 24),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text("Get Started"),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    CircleAvatar(
-                      radius: 28,
-                      backgroundColor: Color.fromRGBO(26, 26, 26, 1),
-                      foregroundColor: Colors.white,
-                      child: Icon(Icons.arrow_forward),
-                    )
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => TaskAppHomePage(),
+                  ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(253, 244, 133, 1),
+                    borderRadius: BorderRadius.circular(48),
+                  ),
+                  padding: const EdgeInsets.only(
+                    left: 32,
+                    top: 4,
+                    bottom: 4,
+                    right: 4,
+                  ),
+                  margin: const EdgeInsets.only(bottom: 24),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text("Get Started"),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      CircleAvatar(
+                        radius: 28,
+                        backgroundColor: Color.fromRGBO(26, 26, 26, 1),
+                        foregroundColor: Colors.white,
+                        child: Icon(Icons.arrow_forward),
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
