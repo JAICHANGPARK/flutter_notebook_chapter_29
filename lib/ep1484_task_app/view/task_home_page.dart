f439aa88dc460dd9e2872cc339e2822cf58d4bce
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:36:23 +0900

    Create task_home_page.dart

---
+++ lib/ep1484_task_app/view/task_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TaskAppHomePage extends StatefulWidget {
+  const TaskAppHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskAppHomePage> createState() => _TaskAppHomePageState();
+}
+
+class _TaskAppHomePageState extends State<TaskAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Color.fromRGBO(26, 26, 26, 1),
+    );
+  }
+}
