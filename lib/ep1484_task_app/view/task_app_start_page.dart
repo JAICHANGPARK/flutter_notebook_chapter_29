9dd8f2580747a109484b8f4507a7963784a6b564
JAICHANGPARK <aristojeff@gmail.com>
2023-04-12 20:23:06 +0900

    Create task_app_start_page.dart

---
+++ lib/ep1484_task_app/view/task_app_start_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskAppStartPage extends StatefulWidget {
+  const TaskAppStartPage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskAppStartPage> createState() => _TaskAppStartPageState();
+}
+
+class _TaskAppStartPageState extends State<TaskAppStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
