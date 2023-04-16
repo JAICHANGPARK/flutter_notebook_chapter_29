4771f58fa63dffa011e8f77a93e4599b598f6c54
JAICHANGPARK <aristojeff@gmail.com>
2023-04-16 09:32:06 +0900

    Update streaming_main_page.dart

---
+++ lib/ep1488_streaming_sns_app/view/streaming_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class StreamingSnsMainPage extends StatefulWidget {
+  const StreamingSnsMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<StreamingSnsMainPage> createState() => _StreamingSnsMainPageState();
+}
+
+class _StreamingSnsMainPageState extends State<StreamingSnsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
