4802a1f89f5ecc057e108b72b942939f8def638b
JAICHANGPARK <aristojeff@gmail.com>
2023-04-16 09:32:20 +0900

    Update ep1488_main.dart

--- lib/ep1488_streaming_sns_app/ep1488_main.dart
+++ lib/ep1488_streaming_sns_app/ep1488_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_29/ep1488_streaming_sns_app/view/streaming_main_page.dart';
 
 void main() {
   runApp(StreamingSnsApp());
@@ -9,6 +10,8 @@ class StreamingSnsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: StreamingSnsMainPage(),
+    );
   }
 }
