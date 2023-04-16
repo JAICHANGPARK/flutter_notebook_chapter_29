35ba0172998ea72c3fc756d022e65dc0e82236a3
JAICHANGPARK <aristojeff@gmail.com>
2023-04-16 09:30:55 +0900

    Create ep1488_main.dart

---
+++ lib/ep1488_streaming_sns_app/ep1488_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class StreamingSnsApp extends StatelessWidget {
+  const StreamingSnsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
