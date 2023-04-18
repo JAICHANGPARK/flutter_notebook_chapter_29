a3b0a4bbbf2e04587510e8ca125f8dcb9b3aad9f
JAICHANGPARK <aristojeff@gmail.com>
2023-04-18 22:47:23 +0900

    Create streaming_profile_page.dart

---
+++ lib/ep1488_streaming_sns_app/view/streaming_profile_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class StreamingProfilePage extends StatefulWidget {
+  const StreamingProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<StreamingProfilePage> createState() => _StreamingProfilePageState();
+}
+
+class _StreamingProfilePageState extends State<StreamingProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
