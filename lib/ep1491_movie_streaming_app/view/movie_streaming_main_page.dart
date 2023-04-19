e42551d06cfbc825421ee8ae2a46de107bf16784
JAICHANGPARK <aristojeff@gmail.com>
2023-04-19 18:24:18 +0900

    Update movie_streaming_main_page.dart

--- lib/ep1491_movie_streaming_app/view/movie_streaming_main_page.dart
+++ lib/ep1491_movie_streaming_app/view/movie_streaming_main_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class MovieStreamingMainPage extends StatefulWidget {
+  const MovieStreamingMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MovieStreamingMainPage> createState() => _MovieStreamingMainPageState();
+}
+
+class _MovieStreamingMainPageState extends State<MovieStreamingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
