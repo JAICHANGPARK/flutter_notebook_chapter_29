625e86d193f247bf06ba7c0334dcb63f8063f5e9
JAICHANGPARK <aristojeff@gmail.com>
2023-04-19 18:28:27 +0900

    Update movie_streaming_main_page.dart

--- lib/ep1491_movie_streaming_app/view/movie_streaming_main_page.dart
+++ lib/ep1491_movie_streaming_app/view/movie_streaming_main_page.dart
@@ -11,16 +11,28 @@ class _MovieStreamingMainPageState extends State<MovieStreamingMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 0,
-          top: 0,
-          child: Container(),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+
+          ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
