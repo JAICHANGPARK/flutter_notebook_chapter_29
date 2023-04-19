30b94cd2f38b91643667cc7de3bc3a6b7c93cebc
JAICHANGPARK <aristojeff@gmail.com>
2023-04-19 18:23:40 +0900

    Update movie_streaming_app.dart

--- lib/ep1491_movie_streaming_app/view/movie_streaming_app.dart
+++ lib/ep1491_movie_streaming_app/view/movie_streaming_app.dart
@@ -9,6 +9,8 @@ class MovieStreamingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: ,
+    );
   }
 }
