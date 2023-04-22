e76f5121e18b5f4a8efc8d786513c353e916bc04
JAICHANGPARK <aristojeff@gmail.com>
2023-04-22 09:02:48 +0900

    Update ep1494_main.dart

--- lib/ep1494_shopping_app/ep1494_main.dart
+++ lib/ep1494_shopping_app/ep1494_main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+void main() {}
 
-void main() {
-  
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
 }
