4e3f4408611f19479db0c9ca62d56655bb382386
JAICHANGPARK <aristojeff@gmail.com>
2023-04-22 09:05:05 +0900

    Update ep1494_main.dart

--- lib/ep1494_shopping_app/ep1494_main.dart
+++ lib/ep1494_shopping_app/ep1494_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_29/ep1494_shopping_app/view/shopping_home_page.dart';
 
 void main() {
   runApp(ShoppingApp());
@@ -9,6 +10,10 @@ class ShoppingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => ShoppingHomePage(),
+      },
+    );
   }
 }
