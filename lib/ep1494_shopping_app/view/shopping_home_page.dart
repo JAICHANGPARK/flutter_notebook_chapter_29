8639248df17cb7d102b5a92442297e650556c1aa
JAICHANGPARK <aristojeff@gmail.com>
2023-04-22 09:03:53 +0900

    Create shopping_home_page.dart

---
+++ lib/ep1494_shopping_app/view/shopping_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ShoppingHomePage extends StatefulWidget {
+  const ShoppingHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingHomePage> createState() => _ShoppingHomePageState();
+}
+
+class _ShoppingHomePageState extends State<ShoppingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
