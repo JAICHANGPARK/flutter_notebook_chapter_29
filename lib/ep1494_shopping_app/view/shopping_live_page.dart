6b5f6a30ac66cf50b8bcafedab8a1614eba768d4
JAICHANGPARK <aristojeff@gmail.com>
2023-04-24 18:23:54 +0900

    Update shopping_live_page.dart

---
+++ lib/ep1494_shopping_app/view/shopping_live_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingLivePage extends StatefulWidget {
+  const ShoppingLivePage({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingLivePage> createState() => _ShoppingLivePageState();
+}
+
+class _ShoppingLivePageState extends State<ShoppingLivePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
