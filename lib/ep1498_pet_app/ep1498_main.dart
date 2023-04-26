471e103840f28a6b1f65c93d8b5b54162c832555
JAICHANGPARK <aristojeff@gmail.com>
2023-04-26 19:08:32 +0900

    Update ep1498_main.dart

--- lib/ep1498_pet_app/ep1498_main.dart
+++ lib/ep1498_pet_app/ep1498_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const PetApp());
+}
 
 class PetApp extends StatelessWidget {
   const PetApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp();
   }
 }
