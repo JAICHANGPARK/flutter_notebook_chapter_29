b5af27033583db18c8b8e2dfefed2dc52cb68211
JAICHANGPARK <aristojeff@gmail.com>
2023-04-26 19:09:22 +0900

    Create pet_home_page.dart

---
+++ lib/ep1498_pet_app/view/pet_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PetHomePage extends StatefulWidget {
+  const PetHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<PetHomePage> createState() => _PetHomePageState();
+}
+
+class _PetHomePageState extends State<PetHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
