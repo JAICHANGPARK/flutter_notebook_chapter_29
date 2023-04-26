80f97aa7dab7c141be9ba14172072e238f3cc27a
JAICHANGPARK <aristojeff@gmail.com>
2023-04-26 19:13:33 +0900

    Update pet_home_page.dart

--- lib/ep1498_pet_app/view/pet_home_page.dart
+++ lib/ep1498_pet_app/view/pet_home_page.dart
@@ -15,11 +15,21 @@ class _PetHomePageState extends State<PetHomePage> {
         child: Stack(
           children: [
             Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: Column(
+                children: [
+
+                ],
+              ),
+            ),
+            Positioned(
               bottom: 0,
               left: 0,
               right: 0,
               child: Container(
-                
                 decoration: BoxDecoration(
                   color: Colors.white,
                 ),
