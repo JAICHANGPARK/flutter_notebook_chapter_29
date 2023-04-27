c1e3bd0e6bea43afc351155edfa21ccc3019c32b
JAICHANGPARK <aristojeff@gmail.com>
2023-04-27 09:36:31 +0900

    Update pet_home_page.dart

--- lib/ep1498_pet_app/view/pet_home_page.dart
+++ lib/ep1498_pet_app/view/pet_home_page.dart
@@ -29,34 +29,34 @@ class _PetHomePageState extends State<PetHomePage> {
                           radius: 24,
                           backgroundColor: Colors.grey[200],
                           foregroundColor: Colors.black,
-                          child: Icon(Icons.apps),
+                          child: const Icon(Icons.apps),
                         ),
                         Expanded(
                           child: Container(
                             height: 48,
-                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            margin: const EdgeInsets.symmetric(horizontal: 16),
                             decoration: BoxDecoration(
                               border: Border.all(
                                 color: Colors.grey[500]!,
                               ),
                               borderRadius: BorderRadius.circular(32),
                             ),
-                            padding: EdgeInsets.all(4),
+                            padding: const EdgeInsets.all(4),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Icon(
+                                const Icon(
                                   Icons.location_on,
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   width: 8,
                                 ),
-                                Text("3740 Clover Penrose..."),
+                                const Text("3740 Clover Penrose..."),
                               ],
                             ),
                           ),
                         ),
-                        CircleAvatar(
+                        const CircleAvatar(
                           radius: 24,
                         ),
                       ],
@@ -79,48 +79,48 @@ class _PetHomePageState extends State<PetHomePage> {
                         color: Colors.black.withOpacity(0.1),
                         blurRadius: 10,
                         spreadRadius: 6,
-                        offset: Offset(0, 6),
+                        offset: const Offset(0, 6),
                       )
                     ],
                   ),
                   height: 64,
-                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       IconButton(
                         onPressed: () {},
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.home_filled,
                         ),
                         color: Colors.black,
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 16,
                       ),
                       IconButton(
                         onPressed: () {},
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.send,
                         ),
                         color: Colors.grey,
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 16,
                       ),
                       IconButton(
                         onPressed: () {},
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.favorite,
                         ),
                         color: Colors.grey,
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 16,
                       ),
                       IconButton(
                         onPressed: () {},
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.chat,
                         ),
                         color: Colors.grey,
