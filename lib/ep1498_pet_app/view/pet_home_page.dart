98c68b56c3c859221ae25b81ba94050e4242e696
JAICHANGPARK <aristojeff@gmail.com>
2023-04-28 16:13:15 +0900

    Update pet_home_page.dart

--- lib/ep1498_pet_app/view/pet_home_page.dart
+++ lib/ep1498_pet_app/view/pet_home_page.dart
@@ -176,26 +176,26 @@ class _PetHomePageState extends State<PetHomePage> {
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) {
                           return Container(
-                              width: 300,
-                              margin: EdgeInsets.only(right: 16),
-                              decoration: BoxDecoration(
-                                color: Colors.orange,
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                              child: Stack(
-                                children: [
-                                  Positioned.fill(
-
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(32),
-                                      child: Image.network(
-                                        "https://cdn.pixabay.com/photo/2020/10/06/11/50/dog-5632005_1280.jpg",
-                                        fit: BoxFit.cover,
-                                      ),
+                            width: 300,
+                            margin: EdgeInsets.only(right: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.circular(32),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned.fill(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(32),
+                                    child: Image.network(
+                                      "https://cdn.pixabay.com/photo/2020/10/06/11/50/dog-5632005_1280.jpg",
+                                      fit: BoxFit.cover,
                                     ),
-                                  )
-                                ],
-                              ));
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
                         },
                       ),
                     ),
